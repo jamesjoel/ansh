e6d0cb8d2b8267bd2b927a82119eb4845c38fe5d
jamesjoel <james.joelj@gmail.com>
2025-06-12 12:46:06 +0530

    js day1

---
+++ nodejs/intro.js
@@ -0,0 +1,40 @@
+/*
+case-sen
+
+    Variables
+        let
+        var
+        const
+
+*/
+let a = 10
+let b = 20 
+let c = a+b 
+console.log(c)
+
+/*
+.header{
+    color : red;font-size : 20;
+}
+
+<h1>hello</h1><h1>wlrodl</h1>
+
+
+Rules for Naming the Variables
+
+    1. spacial char not allwoed (_, $)
+    2. number start not allowed
+    3. keyword 
+    4. name case-sen
+
+
+    let a = 
+    let a.a = 
+    let a a = 
+    let a_a =
+    let a$a = 
+    let _a = 
+    let __a = 
+    let _ = 
+
+*/
